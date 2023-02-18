a1666c1ee7837561721be6317777bec516c7e859
HunterMarion <99689649+HunterMarion@users.noreply.github.com>
2023-02-18 00:29:30 -0600

    Demo "About" and "Reservations" pages.

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class About extends React.Component {
+
+     render() {
+          return (<>
+              <h1>WELCOME</h1>
+              <p>This is morning snack's about us page</p>
+          </>  
+          )  
+     }
+}
+
+export default About;
---
+++ src/pages/Reservations.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Reservations extends React.Component {
+
+     render() {
+          return (<>
+              <h1>WELCOME</h1>
+              <p>This is morning snack's reservations page</p>
+          </>  
+          )  
+     }
+}
+
+export default Reservations;
