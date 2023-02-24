f74425701769b17e177965dee0cd04ee7be748e0
michellenicole23 <64987167+michellenicolephillips@users.noreply.github.com>
2023-02-23 22:22:17 -0600

    descriptions for lunch catering and pricing for all

--- src/pages/Catering.js
+++ src/pages/Catering.js
@@ -7,28 +7,38 @@ class Catering extends React.Component {
               <div>
                    <h1>Catering</h1>
                    <p>We offer both breakfast and lunch catering options for your event.</p>
-                   <h3>Breakfast</h3>
-                   <h4>Tier 1 - Morning Pastries Package</h4>
-                   <p> An assortment of muffins, bagels, danishes, croissants, served with butter and a variety of jams. Coffee and water available for beverages</p>
-                   <h4>Tier 2 - American Breakfast Package</h4>
-                   <p>Scrambled Eggs, bacon, breakfast sausage, southwestern breakfast potatoes, and an assortment of pastries. Coffee, your choice of 2 types of juices, and water available for beverages</p>
-                   <h4>Add On- Omelette Station</h4>
-                   <p>An omelette chef or two, depending on your event size, will be provided for the omelette station. Below are the filling options.</p>
-                   <ul>
-                        <li>Proteins: Bacon, Pork Sausage, Turkey Sausage, Chicken, Vegan Sausage, Tofu</li>
-                        <li>Vegetables: Tomato, Onion, Mushroom, Jalapeno, Red and Green Bell Pepper, Spinach, Avocado, Green Onion</li>
-                        <li>Cheeses: Cheddar, Swiss, Mozzarella, Pepper Jack</li>
-                   </ul>
-                   <h3>Lunch</h3>
-                   <h4>Tier 1</h4>
-                   <p>Light Lunch</p>
-                   <p> Sandwiches, soups or salads, coffee, fountain drink or tea</p>
-                   <h4>Tier 2</h4>
-                   <p>Plated Lunch</p>
-                   <p>Burgers, fries, fountain drinks or tea, with sides</p>
-                   <h4>Add Ons</h4>
-                   <p>Desserts</p>
-                   <p>*Vegan and allergy accomodations available upon request</p>
+                   <div id="breakfast-catering">
+                         <h3>Breakfast</h3>
+                         <h4>Tier 1 - Morning Pastries Package - $15 per person</h4>
+                         <p> An assortment of muffins, bagels, danishes, croissants, served with butter and a variety of jams. Coffee and water available for beverages</p>
+                         <h4>Tier 2 - American Breakfast Package - $25 per person</h4>
+                         <p>Scrambled Eggs, bacon, breakfast sausage, southwestern breakfast potatoes, and an assortment of pastries. Coffee, your choice of 2 types of juices, and water available for beverages</p>
+                         <h4>Add On- Omelette Station - $20 per person</h4>
+                         <p>A number of omelette chefs will be provided for the omelette station, varying based on event size. Below are the filling options.</p>
+                         <ul>
+                              <li>Proteins: Bacon, Pork Sausage, Turkey Sausage, Chicken, Vegan Sausage, Tofu</li>
+                              <li>Vegetables: Tomato, Onion, Mushroom, Jalapeno, Red & Green Bell Pepper, Spinach, Avocado, Green Onion</li>
+                              <li>Cheeses: Cheddar, Swiss, Mozzarella, Pepper Jack</li>
+                         </ul>
+                         <h4>Add On - Fruit $10 per person</h4>
+                         <p>An assortment of apples, pears, bananas, and oranges and a berry meledy</p>
+                   </div>
+                   <div id="lunch-catering">
+                         <h3>Lunch</h3>
+                         <h4>Tier 1 - Light Lunch Package - $20 per person</h4>
+                         <p> An assortment of sandwiches, 2 soups of your choice, and caesar salad. Iced Tea, Coke Products, and Water avaialble for beverages</p>
+                         <ul>
+                              <li>Sandwiches - Turkey & Swiss, Ham & Cheddar, Fresh Vegetables & Hummus</li>
+                              <li>Soups- Tomato Basil, Tortilla, French Onion, Seasonal</li>
+                         </ul>
+                         <h4>Tier 2 - Burger Package $30 per person</h4>
+                         <p>Beef or black bean patties, American Chees, Lettuce, Tomato, Onion, Pickles served on a brioche bung. Fries and caesar salad for sides. Iced Tea, Coke Products, and water available for beverages</p>
+                         <h4>Add On - Desserts - $15 per person</h4>
+                         <p>Choclate chip, oatmeal rasin, and snickerdoodle cookies, chocolate brownies</p>
+                         <h4>Add On - Fruit - $10 per person</h4>
+                              <p>An assortment of apples, pears, bananas, and oranges and a berry meledy</p>
+                   </div>
+                   <p id="catering-dietary">*Vegan and allergy accomodations available upon request</p>
               </div>
           )  
      }
