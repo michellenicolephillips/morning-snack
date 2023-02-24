36adc6eb0ead2a6c72042f22549d0199ced7adad
michellenicole23 <64987167+michellenicolephillips@users.noreply.github.com>
2023-02-23 20:04:06 -0600

    basic catering menu with breakfast descriptions

--- src/pages/Catering.js
+++ src/pages/Catering.js
@@ -3,10 +3,28 @@ import React from 'react';
 class Catering extends React.Component {
 
      render() {
-          return (<>
-              <h1>WELCOME</h1>
-              <p>This is morning snack's catering page</p>
-          </>  
+          return (
+              <div>
+                   <h1>Catering</h1>
+                   <p>We offer both breakfast and lunch catering options for your event.</p>
+                   <h3>Breakfast</h3>
+                   <h4>Tier 1 - Morning Pastries Package</h4>
+                   <p> An assortment of muffins, bagels, danishes, croissants, served with butter and a variety of jams. Coffee and water available for beverages</p>
+                   <h4>Tier 2 - American Breakfast Package</h4>
+                   <p>Scrambled Eggs, bacon, breakfast sausage, southwestern breakfast potatoes, and an assortment of pastries. Coffee, your choice of 2 types of juices, and water available for beverages</p>
+                   <h4>Add Ons</h4>
+                   <p>Omelette Station</p>
+                   <h3>Lunch</h3>
+                   <h4>Tier 1</h4>
+                   <p>Light Lunch</p>
+                   <p> Sandwiches, soups or salads, coffee, fountain drink or tea</p>
+                   <h4>Tier 2</h4>
+                   <p>Plated Lunch</p>
+                   <p>Burgers, fries, fountain drinks or tea, with sides</p>
+                   <h4>Add Ons</h4>
+                   <p>Desserts</p>
+                   <p>*Vegan and allergy accomodations available upon request</p>
+              </div>
           )  
      }
 }
