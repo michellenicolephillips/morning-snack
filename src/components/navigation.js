faf937071bbbc471a86c50fc91655f089d276546
HunterMarion <99689649+HunterMarion@users.noreply.github.com>
2023-04-02 17:27:43 -0500

    Hunter fix reservations (#34)
    * fixing reservations popup 01
    
    * fix reservations popup
    
    * reservations popup
    
    ---------
    
    Co-authored-by: Michelle <64987167+michellenicolephillips@users.noreply.github.com>

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,12 +8,12 @@ class Navigation extends React.Component {
           this.state = {
                show: false
           };
+          this.handleClose = this.handleClose.bind(this);
+          this.handleShow = this.handleShow.bind(this);
      }
 
      handleClose = () => this.setState({ show: false });
      handleShow = () => this.setState({ show: true });
-
-
 
      render() {
           return (
@@ -33,47 +33,42 @@ class Navigation extends React.Component {
                          </NavItem>
 
                          <Button variant="warning" className="reserveButton" onClick={this.handleShow}><strong>BOOK A RESERVATION</strong></Button>
-                         
-                         <div className="reservePopup">
-                              <Modal
-                                   show={this.show}
-                                   onHide={this.handleClose}
-                                   backdrop="static"
-                                   keyboard={false}
-                              >
-                                   <Modal.Body>
-                                        <Form>
-                                             <Form.Group className="mb-3">
-                                                  <Row>
-                                                       <Form.Group as={Col}>
-                                                            <Form.Label>Guests</Form.Label>
-                                                            <Form.Control type="number" placeholder="Number of Guests" />
-                                                       </Form.Group>
+                         <Modal
+                              show={this.state.show}
+                              onHide={this.handleClose}
+                              backdrop="static"
+                              keyboard={false}
+                         >
+                              <Modal.Body>
+                                   <Form>
+                                        <Form.Group className="mb-3">
+                                             <Row>
+                                                  <Form.Group as={Col}>
+                                                       <Form.Label>Guests</Form.Label>
+                                                       <Form.Control type="number" placeholder="Number of Guests" />
+                                                  </Form.Group>
 
-                                                       <Form.Group as={Col}>
-                                                            <Form.Label>Date</Form.Label>
-                                                            <Form.Control type="date" placeholder="Enter date" />
-                                                       </Form.Group>
+                                                  <Form.Group as={Col}>
+                                                       <Form.Label>Date</Form.Label>
+                                                       <Form.Control type="date" placeholder="Enter date" />
+                                                  </Form.Group>
 
-                                                       <Form.Group as={Col}>
-                                                            <Form.Label>Time</Form.Label>
-                                                            <Form.Control type="time" min="10:00" max="17:00" placeholder="Enter time" />
-                                                       </Form.Group>
+                                                  <Form.Group as={Col}>
+                                                       <Form.Label>Time</Form.Label>
+                                                       <Form.Control type="time" min="10:00" max="17:00" placeholder="Enter time" />
+                                                  </Form.Group>
 
-                                                       <Form.Group as={Col}>
-                                                            <Button variant="danger" type="submit">Find a Table</Button>
-                                                       </Form.Group>
-                                                  </Row>
-                                             </Form.Group>
-                                        </Form>
-                                   </Modal.Body>
-                                   <Modal.Footer>
-                                        <Button variant="secondary" onClick={this.handleClose}>
-                                             Close
-                                        </Button>
-                                   </Modal.Footer>
-                              </Modal>
-                         </div>
+                                                  <Form.Group as={Col}>
+                                                       <Button variant="danger" type="submit">Find a Table</Button>
+                                                  </Form.Group>
+                                             </Row>
+                                        </Form.Group>
+                                   </Form>
+                              </Modal.Body>
+                              <Modal.Footer>
+                                   <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                              </Modal.Footer>
+                         </Modal>
                     </Nav>
                </>
           )
